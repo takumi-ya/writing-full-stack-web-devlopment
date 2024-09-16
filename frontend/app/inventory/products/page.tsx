1b0a87c2110c5c93fbb5c3f517b68dada7c56371
Takumi Yamada <110591229+takumi-ya@users.noreply.github.com>
2024-09-16 12:52:57 +0900

    Create mockup of product list page

--- frontend/app/inventory/products/page.tsx
+++ frontend/app/inventory/products/page.tsx
@@ -1,8 +1,50 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import productsData from "./sample/dummy_products.json";
+
+type ProductData = {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+};
+
 export default function Page() {
+    const [ data, setData] = useState<Array<ProductData>>([]);
+
+    useEffect(() => {
+        setData(productsData);
+    }, []);
+
     return (
         <div>
             <h2>商品一覧</h2>
-            <p>商品の一覧を表示</p>
+            <button>商品を追加する</button>
+            <table>
+                <thead>
+                    <tr>
+                        <th>商品ID</th>
+                        <th>商品名</th>
+                        <th>単価</th>
+                        <th>説明</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((data: any) => (
+                        <tr key={data.id}>
+                            <td>{data.id}</td>
+                            <td>{data.name}</td>
+                            <td>{data.price}</td>
+                            <td>{data.description}</td>
+                            <td>
+                                <button>更新・削除</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
