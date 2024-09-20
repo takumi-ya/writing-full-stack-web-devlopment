cdf13ca3f235b3dc15cc08d8a0824e8d5094e51c
Takumi Yamada <110591229+takumi-ya@users.noreply.github.com>
2024-09-20 06:35:33 +0000

    Add Link to products screen

--- frontend/app/inventory/products/page.tsx
+++ frontend/app/inventory/products/page.tsx
@@ -1,50 +1,55 @@
-'use client'
+"use client";
 
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import productsData from "./sample/dummy_products.json";
 
 type ProductData = {
-    id: number;
-    name: string;
-    price: number;
-    description: string;
+  id: number;
+  name: string;
+  price: number;
+  description: string;
 };
 
 export default function Page() {
-    const [ data, setData] = useState<Array<ProductData>>([]);
+  const [data, setData] = useState<Array<ProductData>>([]);
 
-    useEffect(() => {
-        setData(productsData);
-    }, []);
+  useEffect(() => {
+    setData(productsData);
+  }, []);
 
-    return (
-        <div>
-            <h2>商品一覧</h2>
-            <button>商品を追加する</button>
-            <table>
-                <thead>
-                    <tr>
-                        <th>商品ID</th>
-                        <th>商品名</th>
-                        <th>単価</th>
-                        <th>説明</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((data: any) => (
-                        <tr key={data.id}>
-                            <td>{data.id}</td>
-                            <td>{data.name}</td>
-                            <td>{data.price}</td>
-                            <td>{data.description}</td>
-                            <td>
-                                <button>更新・削除</button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    )
+  return (
+    <div>
+      <h2>商品一覧</h2>
+      <button>商品を追加する</button>
+      <table>
+        <thead>
+          <tr>
+            <th>商品ID</th>
+            <th>商品名</th>
+            <th>単価</th>
+            <th>説明</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((data: any) => (
+            <tr key={data.id}>
+              <td>{data.id}</td>
+              <td>{data.name}</td>
+              <td>{data.price}</td>
+              <td>{data.description}</td>
+              <td>
+                <Link href={"/inventory/products/${data.id}"}>在庫処理</Link>
+              </td>
+              <td>
+                <button>更新・削除</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
